d8e8cfbda9e9d5994171428c82495137a0745ca0
MuneebMoosa <muneebmoosavv@gmail.com>
2025-07-25 12:04:59 +0530

    strucure of add a new book added

--- src/HomePage/Home.jsx
+++ src/HomePage/Home.jsx
@@ -10,6 +10,35 @@ const Home = () => {
           <button className="home-btn first-btn">
             Add a new book 
           </button>
+
+          <div className="add-container">
+              <form action="">
+                  <label htmlFor='title'>Book Title</label>
+                  <br />
+                  <input id='title' type="text" />
+
+                  <br />
+
+                  <label  htmlFor='author'>Author</label>
+                  <br />
+                  <input  id='author' type="text" />
+
+                  <br />
+
+                  <label >Status</label>
+                  <br />
+                  <input type="radio" name="status" id="option1" />
+                  <label htmlFor="option1">Read</label>
+
+                  <input type="radio" name="status" id="option2" />
+                  <label htmlFor="option2">Currently Reading</label>
+
+                  <input type="radio" name="status" id="option3" />
+                  <label htmlFor="option3">want To Read</label>
+               
+              </form>
+          </div>
+
           <button  className="home-btn second-btn">
                 My Library
           </button>
