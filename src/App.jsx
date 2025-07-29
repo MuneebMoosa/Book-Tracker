8b6b39a5df252a6a241f566b46242d76ca582565
MuneebMoosa <muneebmoosavv@gmail.com>
2025-07-29 21:17:13 +0530

    Detail Page Added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import "./index.css";
 import Home from './HomePage/Home';
 import Library from './LibraryPage/Library';
+import Detail from './DetailsPage/Detail';
 const App = () => {
   return (
     <div>
       {/* <Home/> */}
-      <Library/>
+      {/* <Library/> */}
+      <Detail/>
     </div>
   )
 }
